c3ce56e6b6e5d265d57a5c0baedf10e268d307cd
Magnus Gudmundsson <cyberdada@cyberdada.com>
2013-12-27 22:12:11 +0100

    week browsing working

--- js/controllers/calendarHead.js
+++ js/controllers/calendarHead.js
@@ -1,19 +1,36 @@
 //var myApp = angular.module('myApp',[]);
 
 function calendarHead($scope) {
- $scope.scale = "week";
- $scope.days = initDays();
+ 	$scope.scale = "week";
+	var _mon =  moment().subtract('days', moment().day() - 1);
+ 	$scope.days = initDays();
 
 
+	function initDays() {
+		moment.lang('sv');    
+		var retval = [];
+		var pon = _mon;
+		for(var i = 0;i < 7 ; i++ )	{
+				retval.push(pon.format("ddd, MMM, Do YYYY"));
+			 	pon = moment(pon).add('days', 1);
+			}
+		return retval;
+	};
 
-function initDays() {
-	moment.lang('sv');
-var mon = moment().subtract('days', moment().day() - 1);
-var retval = [];
-for(var i = 0;i < 7 ; i++ )	{
-		retval.push(mon.format("ddd, MMM, Do YYYY"));
-	 	mon = moment(mon).add('days', 1);
+
+	$scope.dateNow = function(){
+		_mon =  moment().subtract('days', moment().day() - 1);
+ 		$scope.days = initDays();
 	}
-return retval;
-} 
+
+	$scope.nextWeek = function() {
+		_mon = moment(_mon).add('days', 7); 
+ 		$scope.days = initDays();
+	}
+
+	$scope.previousWeek = function() {
+		_mon = moment(_mon).subtract('days', 7); 
+ 		$scope.days = initDays();
+
+	}
 }
