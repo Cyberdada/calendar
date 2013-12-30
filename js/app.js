61590a22041a44309e88a9f9206ab9e4a99a972f
Magnus Gudmundsson <cyberdada@cyberdada.com>
2013-12-30 19:39:12 +0100

    lock working

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ angular.module('CyberDaDaCalendar', [ 'firebase'])
  
 .value('fbURL', 'https://cyberdada.firebaseio.com/')
  
-.factory('Projects', function($firebase, fbURL) {
+.factory('Booked', function($firebase, fbURL) {
   return $firebase(new Firebase(fbURL));
+})
+
+
+.factory('Timeslots', function() {
+	return [{id:1, name:"08:00 - 10:00"}, {id:2, name:"10:00 - 12:00"}, {id:3, name:"12:00 - 14:00"}, {id:4, name:"14:00 - 17:00"}, {id:5,name:"17:00 - 20:00"}, {id:6,name:"20:00 - 22:00"}];
+})
+
+.factory('Bookables', function() {
+	return [{id:1, name:"Tvättmaskin #1"}, {id:2, name:"Tvättmaskin #2"}, {id:3,name:"Tvättmaskin #3"}];
 })
--- js/controllers/calendarHead.js
+++ js/controllers/calendarHead.js
@@ -1,36 +1,76 @@
 //var myApp = angular.module('myApp',[]);
 
-function calendarHead($scope) {
- 	$scope.scale = "week";
-	var _mon =  moment().subtract('days', moment().day() - 1);
- 	$scope.days = initDays();
+function calendarHead($scope, Timeslots,Bookables) {
+         $scope.scale = "vecka";
+        var _mon =  moment().subtract('days', moment().day() - 1);
+         $scope.days = initDays();
+         $scope.Timeslots = Timeslots;
+         $scope.Bookables = Bookables;
+         $scope.Current = "";
+         $scope.currentId = "";
+         $scope.alreadyBooked = [];
+       
+        function initDays() {
+                moment.lang('sv');    
+                var retval = [];
+                var pon = _mon;
+                for(var i = 0;i < 7 ; i++ )  {       
+					retval.push(pon);
+                    pon = moment(pon).add('days', 1);
+                }
+                return retval;
+        };
 
-
-	function initDays() {
-		moment.lang('sv');    
-		var retval = [];
-		var pon = _mon;
-		for(var i = 0;i < 7 ; i++ )	{
-				retval.push(pon.format("ddd, MMM, Do YYYY"));
-			 	pon = moment(pon).add('days', 1);
+        function markBooked() {
+        	for(i = 0; i < $scope.alreadyBooked.length; i++ ) {
+				var p = $("#" + $scope.alreadyBooked[i].book_id)[0];
+				if(p !== undefined && p !== null) {
+					p.children[0].style.color = "#d55e00";
+				}
 			}
-		return retval;
-	};
+        };
 
+		var messageListRef = new Firebase('https://cyberdada.firebaseio.com/bookings');
+		messageListRef.on('child_added', function(snapshot) {
+			$scope.alreadyBooked.push(snapshot.val());
+			 markBooked();
+		});
+       
+        $scope.dateNow = function(){
+                _mon =  moment().subtract('days', moment().day() - 1);
+                 $scope.days = initDays();
+                 $scope.$apply();
+                 markBooked();
+        }
 
-	$scope.dateNow = function(){
-		_mon =  moment().subtract('days', moment().day() - 1);
- 		$scope.days = initDays();
-	}
+        $scope.nextWeek = function() {
+                _mon = moment(_mon).add('days', 7); 
+                 $scope.days = initDays();
+                 $scope.$apply();
+                 markBooked();
+        }
 
-	$scope.nextWeek = function() {
-		_mon = moment(_mon).add('days', 7); 
- 		$scope.days = initDays();
-	}
+        $scope.previousWeek = function() {
+                _mon = moment(_mon).subtract('days', 7); 
+                 $scope.days = initDays();
+                 $scope.$apply();
+                 markBooked();
+        }
 
-	$scope.previousWeek = function() {
-		_mon = moment(_mon).subtract('days', 7); 
- 		$scope.days = initDays();
+        $scope.setCurrent = function(ev) {
+        	var target = event.target || event.srcElement;
+        	while(target.nodeName.toUpperCase()  == "SPAN") {
+        		target = target.parentNode;
+        	}
+        	$scope.Current =target.title;
+        	$scope.currentId = target.id;
+        }
 
-	}
+        $scope.Book = function() {
+			var messageListRef = new Firebase('https://cyberdada.firebaseio.com/bookings');
+			var bookref = messageListRef.push();
+			bookref.set({'book_id': $scope.currentId});
+			$("#myModal").modal('hide');
+
+        }
 }
